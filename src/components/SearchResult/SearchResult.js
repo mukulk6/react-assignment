84f5b8109af2d45d6159fd86cce427873eed0c7a
mukul <mukul.kale@digitalmain.com>
2022-04-27 12:55:34 +0530

    alignment fixed for answered

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -4,13 +4,10 @@ import styles from './SearchResult.module.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  { FontAwesomeIcon }  from '@fortawesome/react-fontawesome';
 import moment from 'moment';
 
-
-
-
-const date= moment().format('LL');
+const date= moment().format('MMM DD, YYYY');
 
 const SearchResult = () => ( 
      <div class="container">
@@ -45,14 +42,20 @@ const SearchResult = () => (
     <div className={styles.expertInfo}>
   <img class="rounded-circle" alt="40x40" src={'../../images/sample.jpg'}
           data-holder-rendered="true"></img>
+          <div class="row">
   <div class={styles.userInfo}>
   <span className={styles.userName}>
       Tejal Kunjir
   </span>
   </div>
+  </div>
+  <div class="row">
   <div className={styles.answeredInfo}>
+  
     <span>Answered</span>
     <span>{date}</span>
+   
+  </div>
   </div>
   <span className={styles.userScore}>
   <FontAwesomeIcon icon="fa-solid fa-award" />
