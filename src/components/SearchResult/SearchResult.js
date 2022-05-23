0245487e6b4a8add031275f804d12129c71f7dc0
mukul <mukul.kale@digitalmain.com>
2022-05-23 18:32:51 +0530

    updated

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -8,6 +8,7 @@ import  { FontAwesomeIcon }  from '@fortawesome/react-fontawesome';
 import moment from 'moment';
 
 const date= moment().format('MMM DD, YYYY');
+const text = '<p>Cisco and Pure Storage deliver FlashStack, a modern converged infrastructure (CI) solution that is smarter, simpler, smaller -and more efficient than ever before. FlashStack is virtual machine-aware and hybrid cloud-ready, while retaining the predictability and efficiency advantages of dedicated compute and storage tiers. With FlashStack customers can modernize their operational model, stay ahead of business demands, and protect and secure their applications and data, regardless of the deployment model on premises, at the edge, or in the cloud. THE FLASHSTACK ADVANTAGE • Simple no trade-off architecture eliminates disparate hardware silos. Proven, validated inter-operability and for confident application deployment. Infrastructure for both traditional and converged operating models so you can consolidate operations at your pace • Converged infrastructure for multi-hypervisor, bare metal or container deployments.Built for the cloud, including full integration with cloud platforms from Cisco, VMware, OpenStack and othersAnswered</p>';
 
 const SearchResult = () => ( 
      <div class="container">
@@ -15,10 +16,10 @@ const SearchResult = () => (
   <div class="row">
     <div className={styles.innerDiv}>
       <span className={styles.sharingButton}>
-      <FontAwesomeIcon icon={["fas", "share-nodes"]} />
+      <i class="fa-solid fa-share-nodes"></i>
       </span>
-      <span className={styles.sharingButton}>
-      <FontAwesomeIcon icon="fa-solid fa-heart" />
+      <span className={styles.heartButton}>
+      <i class="fa-solid fa-heart"></i>
       </span>
       <h2 className={styles.headTitle}>What is PaaS</h2>
       <div class="input-group-prepend">
@@ -47,20 +48,32 @@ const SearchResult = () => (
   <span className={styles.userName}>
       Tejal Kunjir
   </span>
+  <div className={styles.paste}>
+  <span> 
+    <i class="fa-solid fa-paste"></i>
+     </span>
+     <span className={styles.reqImg}>
+       <img src=".../images/help-support.jpg"></img>
+     </span>
+     </div>
   </div>
   </div>
   <div class="row">
   <div className={styles.answeredInfo}>
+    <span className='answeredBy'>Answered</span>
+    <span className='datestamp'>{date}</span>
   
-    <span>Answered</span>
-    <span>{date}</span>
+  </div>
+  </div>
+              <span className={styles.userScore}>
+                <FontAwesomeIcon icon="fa-solid fa-award" />
+              </span>
    
+              <div class='row'>
+     <p>Ans text </p>
+   </div>
   </div>
-  </div>
-  <span className={styles.userScore}>
-  <FontAwesomeIcon icon="fa-solid fa-award" />
-  </span>
-  </div>
+ 
   </div>
 </div>
   </div>
