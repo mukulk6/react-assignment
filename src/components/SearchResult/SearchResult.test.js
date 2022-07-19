74e0a3bee32120eb33d4cb19395fd97be832ea43
mukul <mukul.kale@digitalmain.com>
2022-07-19 18:17:22 +0530

    Rolling back commits

--- src/components/SearchResult/SearchResult.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchResult from './SearchResult';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchResult />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
