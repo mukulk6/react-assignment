f4f5cf365a8afbe643de2b9a5fc4d77ccafd6b68
mukul <mukul.kale@digitalmain.com>
2022-07-21 11:13:56 +0530

    Modal CSS added

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchResult.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Modal = () => {
+    
+}
+
+
+Modal.propTypes = {};
+Modal.defaultProps = {};
+export default Modal;
